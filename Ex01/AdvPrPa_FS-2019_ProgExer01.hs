8d6307d57a16981906d41f6bb3ee1c8e35f3d046
Stefan Kapferer <stefan@kapferer.ch>
2019-03-06 13:51:00 +0100

    Solve first exercise

--- Ex01/AdvPrPa_FS-2019_ProgExer01.hs
+++ Ex01/AdvPrPa_FS-2019_ProgExer01.hs
@@ -3,8 +3,6 @@ module ProgExercises.HS_2019_ProgExer01Prob_V01 where
 -- Develop some functions to work with two-dimensional vectors,
 -- using pairs, including pair patterns.
 
-toBeImplemented = undefined
-
 -- a type for two-dimensional vectors
 type Vec = (Double, Double)
 
@@ -25,14 +23,14 @@ exaLengthVec =
   lengthVec a == 3 && lengthVec c == 2
 
 lengthVec :: Vec -> Double
-lengthVec = toBeImplemented
+lengthVec (x, y) = sqrt (x^2 + y^2)
 
 -- negVec negates a vector.
 exaNegVec =
   negVec d == (-3, -4)
 
 negVec :: Vec -> Vec
-negVec = toBeImplemented
+negVec (x, y)  = (-x, -y)
 
 -- negVecCurry negates a vector, but uses currying.
 -- Note: this is a bad use of currying, since
@@ -41,14 +39,14 @@ exaNegVecCurry =
   negVecCurry (fst d) (snd d) == (-3, -4)
 
 negVecCurry :: Double -> Double -> Vec
-negVecCurry = toBeImplemented
+negVecCurry x y  = (-x, -y)
 
 -- addVec adds two vectors.
 exaAddVec =
   a `addVec` b == d
 
 addVec :: Vec -> Vec -> Vec
-addVec = toBeImplemented
+addVec (x1, y1) (x2, y2)  = (x1 + x2, y1 + y2)
 
 -- subVec subtracts two vectors.
 exaSubVec =
@@ -56,7 +54,7 @@ exaSubVec =
 -- Implement this function using negVec and addVec.
 
 subVec :: Vec -> Vec -> Vec
-subVec = toBeImplemented
+subVec v1 v2 = v1 `addVec` negVec v2
 
 -- subVecCurry subtracts two vectors.
 -- Implement this function using negVecCurry and addVec.
@@ -67,7 +65,7 @@ exaSubVecCurry =
   a `subVecCurry` b == (3, -4)
 
 subVecCurry :: Vec -> Vec -> Vec
-subVecCurry = toBeImplemented
+subVecCurry v1 (x2, y2)  = addVec v1 (negVecCurry x2 y2)
 
 -- distance computes the distance between two vectors.
 exaDistance =
@@ -75,12 +73,12 @@ exaDistance =
 -- Implement this function using subVec and lengthVec.
 
 distance :: Vec -> Vec -> Double
-distance = toBeImplemented
+distance v1 v2 = lengthVec (v1 `subVec` v2)
 
 -- Scales a vector with a factor.
 exaScaleVec =
   scaleVec d 3 == (9, 12)
 
 scaleVec :: Vec -> Double -> Vec
-scaleVec = toBeImplemented
+scaleVec (x, y) s = (s * x, s * y)
 
