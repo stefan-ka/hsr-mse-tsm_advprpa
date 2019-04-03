d0ec59e1b45ad99e1ebac670a6a10017d722d88b
Stefan Kapferer <stefan@kapferer.ch>
2019-04-03 10:49:24 +0200

    Implemented exercise 2

--- Ex02/ProgExercise02.hs
+++ Ex02/ProgExercise02.hs
@@ -6,8 +6,6 @@ module ProgExercises.FS_2019_ProgExer02Prob_V01 where
 -- Helpful functions from the Prelude:
 --   and, (!!), zip
 
-toBeImplemented = undefined
-
 -- the type for values
 type Value = Double
 -- the type for matrices
@@ -35,9 +33,10 @@ exaIsMat =
   not (isMat [[1, 2], []])
 
 isMat :: Matrix -> Bool
+isMat [] = True
 isMat (row1 : rows) =
     and [length row == lengthRow1 | row <- rows]
-  where lengthRow1 = length row1
+   where lengthRow1 = length row1
 
 -- Sometimes we use square matrices.
 -- Here is a function that might be used to
@@ -49,14 +48,15 @@ exaIsSquareMat =
   not (isSquareMat [[]]) &&
   not (isSquareMat [[1, 2, 3], [4, 5, 6]])
 
-isSquareMat = isSquareMatV1
+isSquareMat = isSquareMatV2
 
 isSquareMatV1 :: Matrix -> Bool
 isSquareMatV1 [] = True
 isSquareMatV1 matrix = isMat matrix && length (head matrix) == length matrix
 
 isSquareMatV2 :: Matrix -> Bool
-isSquareMatV2 = undefined
+isSquareMatV2 [] = True
+isSquareMatV2 mat@(row1:_) = length row1 == length mat
 
 -- zeroMat generates an m * n zero matrix, that is,
 -- a matrix with all entries 0.
@@ -64,14 +64,16 @@ exaZeroMat =
   zeroMat 2 3 == [[0, 0, 0], [0, 0, 0]]
 
 zeroMat :: Int -> Int -> Matrix
-zeroMat = undefined
+zeroMat rows columns = [zeroRow | _ <- [1..rows]]
+  where
+    zeroRow = [0 | _ <- [1..columns]]
 
 -- Check whether a list of lists represents a zero matrix or not.
 isZeroMatV1 :: Matrix -> Bool
-isZeroMatV1 = undefined
+isZeroMatV1 mat = isMat mat && and [a == 0 | row <- mat, a <-row]
 
 isZeroMatV2 :: Matrix -> Bool
-isZeroMatV2 = undefined
+isZeroMatV2 mat = isMat mat && and [a == 0 | a <- concat mat]
 
 -- unitMat generates an n * n unit matrix, that is,
 -- a square matrix in which all entries are zero,
@@ -83,14 +85,21 @@ exaUnitMat =
 unitMat = unitMatV1
 
 unitMatV1 :: Int -> Matrix
-unitMatV1 = undefined
+unitMatV1 n = [row i | i <- [1..n]]
+  where
+    row i = [if i == j then 1 else 0 | j <- [1..n]]
 
 unitMatV2 :: Int -> Matrix
-unitMatV2 = undefined
+unitMatV2 n = [row i | i <- [1..n]]
+  where
+    row i = [0 | _ <- [1..i-1]] ++ 1 : [0 | _ <- [i+1..n]]
 
--- Check whether two matrices are equal of not.
-equalMat :: Matrix -> Matrix -> Bool
-equalMat = undefined
+-- Check whether two matrices are equal or not.
+equalMatV1 :: Matrix -> Matrix -> Bool
+equalMatV1 mat1 mat2 = isMat mat1 && isMat mat2 && and [x == x' | (x,x') <- zip (concat mat1) (concat mat2)]
+
+equalMatV2 :: Matrix -> Matrix -> Bool
+equalMatV2 = (==)
 
 -- Check whether a list of lists represents a unit matrix or not.
 isUnitMat :: Matrix -> Bool
@@ -102,7 +111,7 @@ exaNeg =
   neg [[1, -2], [0, 4]] == [[-1, 2], [0, -4]]
 
 neg :: Matrix -> Matrix
-neg = undefined
+neg rows = [[-elem | elem <- row] | row <- rows]
 
 -- plusMat adds two matrices, that is,
 -- adds all corresponding components of them.
@@ -114,15 +123,15 @@ exaPlusMat =
 plusMat = plusMatV1
 
 plusMatV1 :: Matrix -> Matrix -> Matrix
-mat1 `plusMatV1` mat2 = undefined
+mat1 `plusMatV1` mat2 = [[a+b | (a, b) <- (zip l1 l2)] | (l1, l2) <- (zip mat1 mat2)]
 
 plusMatV2 :: Matrix -> Matrix -> Matrix
-mat1 `plusMatV2` mat2 = undefined
+mat1 `plusMatV2` mat2 = [zipWith (+) l1 l2 | (l1, l2) <- (zip mat1 mat2)]
 
 -- Reads the element in row i and column j of matrix mat.
 -- precondition: i and j are in range of mat
 readMat :: Matrix -> Int -> Int -> Value
-readMat = undefined
+readMat mat i j = (mat !! i) !! j
 
 -- updateMat 'updates' matrix mat.
 -- The updated matrix is the same as mat, except of
@@ -133,6 +142,8 @@ exaUpdateMat =
   updateMat (unitMat 3) 3 1 5 == [[1,0,0],[0,1,0],[5,0,1]]
 
 updateMat :: Matrix -> Int -> Int -> Value -> Matrix
-updateMat = undefined
+updateMat mat i j val = [if i' == i then updateRow row j else row | (i', row) <- zip [1 ..] mat]
+  where
+    updateRow row j = [if j' == j then val else val' | (j', val') <- zip [1 ..] row]
 
 
